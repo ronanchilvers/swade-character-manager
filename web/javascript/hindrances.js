d2105351d6b86b5b347c7e0483f2a771262476a8
Ronan Chilvers <ronan@d3r.com>
2026-03-22 20:35:15 +0000

    Refactor views to load styles and scripts dynamically; update hindrances UI with JS for better point management

---
+++ web/javascript/hindrances.js
@@ -0,0 +1,41 @@
+// Hindrances javascript
+
+function updatePointSpend() {
+  let points = 4;
+  document.querySelectorAll(".js-hindrance .js-checkbox").forEach((checkbox) => {
+    if (checkbox.checked) {
+      points -= ("minor" == checkbox.value) ? 1 : 2;
+    }
+  });
+  let disableMinor = points < 1 ? true : false;
+  let disableMajor = points < 2 ? true : false;
+  document.querySelectorAll(".js-hindrance .js-checkbox:not(:checked)").forEach((checkbox) => {
+    if ("major" == checkbox.value) {
+      checkbox.disabled = disableMajor;
+    } else {
+      checkbox.disabled = disableMinor;
+    }
+  });
+  document.querySelectorAll(".js-points").forEach((el) => {
+    el.value = points;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  updatePointSpend();
+  document.querySelectorAll(".js-hindrance").forEach((hindrance) => {
+    hindrance.addEventListener('click', (e) => {
+      let t = e.target;
+      if ("LABEL" == t.nodeName) {
+        return;
+      }
+      let was_checked = t.checked;
+      hindrance.querySelectorAll('.js-checkbox').forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+      t.checked = was_checked;
+      updatePointSpend();
+    });
+  });
+
+});
